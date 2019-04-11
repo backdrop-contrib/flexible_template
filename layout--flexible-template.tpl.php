98fac5f40d0e2e68a1ed959ce91c27b9bcc368cf
docwilmot <docwilmot@users.noreply.github.com>
2019-04-10 21:34:07 -0500

    Correct template name

--- layout--flexible-template.tpl.php
+++ layout--flexible-template.tpl.php
@@ -3,6 +3,9 @@
  * @file
  * Template for the Simmons Hero layout.
  *
+ * This template does not function without the Flexible Layout module also
+ * installed and enabled.
+ *
  * Variables:
  * - $title: The page title, for use in the actual HTML content.
  * - $messages: Status and error messages. Should be displayed prominently.
@@ -14,21 +17,9 @@
  * - $attributes: Array of additional HTML attributes to be added to the layout
  *     wrapper. Flatten using backdrop_attributes().
  * - $content: An array of content, each item in the array is keyed to one
- *   region of the layout. This layout supports the following sections:
- *   - $content['header']
- *   - $content['top']
- *   - $content['content']
- *   - $content['sidebar']
- *   - $content['sidebar2']
- *   - $content['third1']
- *   - $content['third2']
- *   - $content['third3']
- *   - $content['quarter1']
- *   - $content['quarter2']
- *   - $content['quarter3']
- *   - $content['quarter4']
- *   - $content['bottom']
- *   - $content['footer']
+ *   region of the layout. This layout supports the region $content['all'] which
+ *   is addapted to providefurther configurable regions, cloumns and rows if the
+ *   Flexible Layout module is installed.
  */
 ?>
 <div class="hero-layouts layout--flexible-template <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
@@ -36,6 +27,19 @@
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
 
-  <?php print $content['all']; ?>
+  <?php if ($messages): ?>
+    <div class="l-messages" role="status" aria-label="<?php print t('Status messages'); ?>">
+      <?php print $messages; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php 
+    if (!module_exists('flexible_layout')) {
+      backdrop_set_message(t('This template requires the <a href="@url">Flexible Layout</a> module for generating configurable regions.', array('@url' => url('https://github.com/backdrop-contrib/flexible_layout'))), 'error');
+    }
+    else {
+      print $output;
+    }
+  ?>
 
 </div><!-- /.layout--flexible-template -->
